4fb51a15d3a59f92e23312c81db87e3f1463c10b
richrobber2 <richardrgguno@hotmail.com>
2021-05-26 23:59:48 -0700

    d

--- configs.ts
+++ configs.ts
@@ -1,10 +1,10 @@
-// Step 1: Remove the `.example` from this file name so it is called `configs.ts` or copy the contents of the `configs.example.ts` file into a new `configs.ts` file
+// Step 1: Remove the `.example` from this file name so it is called `configs.ts` or copy the contents of the `configs.ts` file into a new `configs.ts` file
 // Step 2: Add all your bot's information below. The only required one is token and prefix. NOTE: As long as `.gitignore` file is ignoring configs.ts your configurations will be kept private!
 // Step 3: Remove these comments if you like.
 
 export const configs = {
   // Your bot token goes here
-  token: "",
+  token: "NjQzNDQwMzU5NDM0ODc4OTc3.XclgiA.QUQBiTESwOwwpmAmwKaU21ynyM8",
   // The default prefix for your bot. Don't worry guilds can change this later.
   prefix: "!",
   // This isn't required but you can add bot list api keys here.
@@ -31,9 +31,9 @@ export const configs = {
     // The user ids for the support team
     botSupporters: [] as string[],
     // The user ids for the other devs on your team
-    botDevs: [] as string[],
+    botDevs: ["633668381954015235"] as string[],
     // The user ids who have complete 100% access to your bot
-    botOwners: [] as string[],
+    botOwners: ["633668381954015235",]
   },
   webhooks: {
     // the webhook to use when the bot finds a missing translation
